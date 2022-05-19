f5041d220405bd946a82731e49dda4636b9d4749
jongsun1993 <jongsun1993@gmail.com>
2022-05-19 14:43:31 +0900

    url update

--- article-server/config/urls.py
+++ article-server/config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', include('articles.urls')),
+    path('movies/', include('movies.urls')),
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls'))
 ]
