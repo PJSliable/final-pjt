c422cfa59a6aef307fb5baa9bc2ab832523e957e
jongsun1993 <jongsun1993@gmail.com>
2022-05-20 14:25:19 +0900

    community complete

--- article-server/community/urls.py
+++ article-server/community/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review_list),
-    path('<int:moviePk>/', views.review_create),
-    path('<int:moviePk>/<int:reviewPk>/', views.review_detail_like_or_update_delete),
-    path('review/<int:reviewPk>/', views.create_comment),
-    path('review/<int:reviewPk>/<int:commentPk>/', views.comment_delete),
+    path('', views.review_list_or_create),
+    path('<int:reviewPk>/', views.review_detail_like_or_update_delete),
+    path('comment/', views.create_comment),
+    path('comment/<int:commentPk>/', views.comment_delete),
 ]
