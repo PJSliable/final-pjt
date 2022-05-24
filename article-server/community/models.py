9e9690156ab3d7f6b294ef051ae6fae3510ac3aa
jongsun1993 <jongsun1993@gmail.com>
2022-05-24 11:52:15 +0900

    models/Review.rate update

--- article-server/community/models.py
+++ article-server/community/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.conf import settings
 from movies.models import Movie
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
-    rate = models.IntegerField()
+    rate = models.FloatField(
+    validators=[MinValueValidator(0.0), MaxValueValidator(5.0)],
+    )
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
