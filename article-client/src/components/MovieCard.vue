f67253de34b8cd084c6400df088330b0e027fd7b
jongsun1993 <jongsun1993@gmail.com>
2022-05-24 10:47:05 +0900

    내 영화 추가/삭제 버튼 하트로 변경

--- article-client/src/components/MovieCard.vue
+++ article-client/src/components/MovieCard.vue
@@ -7,9 +7,17 @@
         <div class="flex flex-col justify-evenly" style="width: 100%; height: 100px;">
           <div class="flex justify-end mx-2">
             <form @submit.prevent="clickLike" :data-movie-pk="movie.pk" >
-              <button  class="border-2 border-black px-2" >
-                <span v-if="likestate">시러용</span>
-                <span v-else>조아용</span>
+              <button class="px-2" >
+                <span v-if="likestate">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+                <span v-else>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                  </svg>
+                </span>
               </button>
             </form>
           </div>
