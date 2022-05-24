f475a25a416ff4bad12c2084bd857e48afd50bc2
jongsun1993 <jongsun1993@gmail.com>
2022-05-24 17:55:51 +0900

    movie detail update

--- article-client/src/views/DetailView.vue
+++ article-client/src/views/DetailView.vue
@@ -8,15 +8,18 @@
             <img :src="ptImgUrl" alt="posterImg" class="w-full" style="width:100%; height: 100%;">
           </div>
           <div class="col-span-2 w-full p-3">
-            <div class="flex">
-              <p class="font-bold text-xl md:text-3xl lg:text-4xl">{{ movie.title }}</p>
-            </div>
-
             <div>
-              <p>줄거리</p>
-              <p>{{ movie.overview }}</p>
+              <p class="font-bold my-3 text-xl md:text-3xl lg:text-4xl">{{ movie.title }}</p>
             </div>
 
+            <hr>
+            <div class="my-3 text-2xl">
+              <p>평점 : {{ movie.vote_average }},  개봉일 : {{ movie.release_date }},  장르 : {{ movie.genre_ids }}</p>
+            </div>
+            <div>
+              <p class="my-3 text-2xl">줄거리</p>
+              <p class="text-xl">{{ movie.overview }}</p>
+            </div>
           </div>
         </div>
       </div>
