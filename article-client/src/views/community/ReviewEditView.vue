4f02964a3a93f3d9273aaeb6046d23fc18bf96c8
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-05-25 19:58:09 +0900

    feat reviewEdit_movieTitle

--- article-client/src/views/community/ReviewEditView.vue
+++ article-client/src/views/community/ReviewEditView.vue
@@ -2,7 +2,8 @@
   <div class="flex justify-center items-center mb-10" style="margin-top: 100px">
     <div class="flex w-full justify-center">
       <div class="flex flex-col border-2 border-black bg-white rounded-lg p-5">
-        <p class="flex flex-col text-4xl font-bold p-10 pb-5">리뷰 수정하기</p>
+        <p class="flex flex-col text-4xl font-bold p-10 pb-10">리뷰 수정하기</p>
+        <p class="flex flex-col text-3xl font-Jua">{{review.movie.title}}</p>
         <ReviewForm
           v-if="isReview"
           :review="review"
