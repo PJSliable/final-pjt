dc0c5b28b545ce7d2fa2db106a78b911857808ab
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-05-26 04:54:45 +0900

    feat response carousel

--- article-client/src/components/MovieList.vue
+++ article-client/src/components/MovieList.vue
@@ -1,19 +1,66 @@
 <template>
   <div class="flex flex-col">
     <p class="ml-10 mt-8 font-semibold text-3xl md:text-4xl self-start font-DoHyeon">{{ genreName }} 영화</p>
-    <carousel
-      :items="6"
-      :loop="false"
-      :nav="false"
-    >
-      <MovieCard
-        v-for="(movie, index) in movieList"
-        :key="index"
-        :movie="movie"
-        :isLiked="false"
-        class="mb-12"
-      />
-    </carousel>
+    <div class="sm:hidden">
+      <carousel
+        :items="2"
+        :loop="false"
+        :nav="false"
+      >
+        <MovieCard
+          v-for="(movie, index) in movieList"
+          :key="index"
+          :movie="movie"
+          :isLiked="false"
+          class="mb-12"
+        />
+      </carousel>
+    </div>
+    <div class="hidden sm:inline md:hidden">
+      <carousel
+        :items="3"
+        :loop="false"
+        :nav="false"
+      >
+        <MovieCard
+          v-for="(movie, index) in movieList"
+          :key="index"
+          :movie="movie"
+          :isLiked="false"
+          class="mb-12"
+        />
+      </carousel>
+    </div>
+    <div class="hidden md:inline lg:hidden">
+      <carousel
+        :items="4"
+        :loop="false"
+        :nav="false"
+      >
+        <MovieCard
+          v-for="(movie, index) in movieList"
+          :key="index"
+          :movie="movie"
+          :isLiked="false"
+          class="mb-12"
+        />
+      </carousel>
+    </div>
+    <div class="hidden lg:inline">
+      <carousel
+        :items="6"
+        :loop="false"
+        :nav="false"
+      >
+        <MovieCard
+          v-for="(movie, index) in movieList"
+          :key="index"
+          :movie="movie"
+          :isLiked="false"
+          class="mb-12"
+        />
+      </carousel>
+    </div>
   </div>
 </template>
 
